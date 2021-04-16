4707c5da2ebe1c6f59d3870cd34dae0c3762de3d
signtquater <sightquater@naver.com>
2021-04-16 13:14:32 +0900

    5.3 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기

--- src/main/java/com/FMLearn/book/springboot/web/dto/IndexController.java
+++ src/main/java/com/FMLearn/book/springboot/web/dto/IndexController.java
@@ -1,5 +1,6 @@
 package com.FMLearn.book.springboot.web.dto;
 
+import com.FMLearn.book.springboot.config.auth.dto.SessionUser;
 import com.FMLearn.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,15 +8,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -25,7 +33,8 @@ public class IndexController {
     }
 
     @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model) {PostsResponseDto dto = postsService.findById(id);
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
 
         return "posts-update";

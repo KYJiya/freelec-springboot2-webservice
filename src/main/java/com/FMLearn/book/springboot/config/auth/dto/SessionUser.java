be010837486217863d6c3b1866448c12e34095b1
signtquater <sightquater@naver.com>
2021-04-19 11:20:31 +0900

    5.6 네이버 로그인

--- src/main/java/com/FMLearn/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/FMLearn/book/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.FMLearn.book.springboot.config.auth.dto;
 import com.FMLearn.book.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;

dd729c816fe971920fb213bcc3b48844a9b3841e
signtquater <sightquater@naver.com>
2021-04-28 22:42:05 +0900

    SecurityConfig.java "http .csrf().disable() .authorizeRequests() .any request() .permitAll()"

--- src/main/java/com/FMLearn/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/FMLearn/book/springboot/config/auth/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .authorizeRequests()
                     .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                    .anyRequest().authenticated()
+                    .anyRequest().permitAll()
                 .and()
                     .logout()
                         .logoutSuccessUrl("/")

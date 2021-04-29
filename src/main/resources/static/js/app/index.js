ffcfefe0b7b117d02510965909dfbde8237f9eb7
signtquater <sightquater@naver.com>
2021-04-29 09:39:15 +0900

    SecurityConfig.java, csrf 403 forbidden

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -75,7 +75,6 @@ $(document).ready(function() {
     var header = $("meta[name='_csrf_header']").attr("content");
     $(document).ajaxSend(function(e, xhr, options) {
         xhr.setRequestHeader(header, token);
-        });
     });
 });
 

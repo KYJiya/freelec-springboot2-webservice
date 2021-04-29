bb2659ad50f0757320653ca22e84d20c6dbaee3e
signtquater <sightquater@naver.com>
2021-04-29 09:34:32 +0900

    SecurityConfig.java, csrf 403 forbidden

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -70,4 +70,13 @@ var main = {
 
 };
 
+$(document).ready(function() {
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
+    $(document).ajaxSend(function(e, xhr, options) {
+        xhr.setRequestHeader(header. token);
+        });
+    });
+});
+
 main.init();
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -74,7 +74,7 @@ $(document).ready(function() {
     var token = $("meta[name='_csrf']").attr("content");
     var header = $("meta[name='_csrf_header']").attr("content");
     $(document).ajaxSend(function(e, xhr, options) {
-        xhr.setRequestHeader(header. token);
+        xhr.setRequestHeader(header, token);
         });
     });
 });
